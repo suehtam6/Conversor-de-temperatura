b6c750c5597a7711f59f2777a699b1d77ebb3294
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2025-09-15 14:11:36 -0300

    first commit

---
+++ src/app.java
@@ -0,0 +1,11 @@
+package Temperatura;
+
+public class app {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciandoa o aplicativo...");
+
+        Temperatura novo = new Temperatura();
+
+    }
+}
